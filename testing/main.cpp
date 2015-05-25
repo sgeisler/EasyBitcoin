3a1449501d5e518988c3f143f22cc149117d29c2
sgeisler <geisler.sebastian@googlemail.com>
2015-05-25 14:52:12 +0200

    added unit test for Conversions::fromHex

--- testing/main.cpp
+++ testing/main.cpp
@@ -1,9 +1,29 @@
-#include <iostream>
+#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
+
+#include "catch.hpp"
+
+#include <ByteArray.h>
+#include <Conversions.h>
 
 using namespace std;
 
-int main()
+TEST_CASE("HEX to ByteArray", "[fromHex]")
 {
-    cout << "Hello, World!" << endl;
-    return 0;
-}
+    REQUIRE(Conversions::fromHex("ff")[0] == 255);
+    REQUIRE(Conversions::fromHex("f")[0] == 15 * 16);
+    REQUIRE(Conversions::fromHex("1a")[0] == 26);
+
+    SECTION("Catch wrong chars")
+    {
+        bool worked = false;
+        try
+        {
+            ByteArray b = Conversions::fromHex("1x");
+        }
+        catch (runtime_error &e)
+        {
+            worked = true;
+        }
+        REQUIRE(worked);
+    }
+};
