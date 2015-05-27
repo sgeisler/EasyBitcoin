1ff145f3672fddc1c8d494e5bc1a45c9d095ded7
sgeisler <geisler.sebastian@googlemail.com>
2015-05-27 18:53:51 +0200

    added unit tests for newly implemented features

--- testing/main.cpp
+++ testing/main.cpp
@@ -27,3 +27,47 @@ TEST_CASE("HEX to ByteArray", "[fromHex]")
         REQUIRE(worked);
     }
 };
+
+TEST_CASE("ByteArray to hex", "[toHex]")
+{
+    std::string hex = "123fa4ffd900";
+    ByteArray ba = Conversions::fromHex(hex);
+    REQUIRE(Conversions::toHex(ba) == hex);
+};
+
+TEST_CASE("Decode base58", "[fromBase58]")
+{
+    ByteArray ba = Conversions::fromBase58("1LtyTmydQP28esyweRn2mJog5FkQxRh6LD");
+    REQUIRE(Conversions::toHex(ba) == "00da3ebc5a339c2cdf74584108767c68752e5700513d1c606e");
+};
+
+TEST_CASE("Decode base58 check", "[fromBase58Check]")
+{
+    REQUIRE(Conversions::fromBase58Check("1LtyTmydQP28esyweRn2mJog5FkQxRh6LD", 0) ==
+            Conversions::fromHex("da3ebc5a339c2cdf74584108767c68752e570051"));
+
+    bool worked = true;
+    try
+    {
+        Conversions::fromBase58Check("1rtyTmydQP28esyweRn2mJog5FkQxRh6LD", 0);
+    }
+    catch (std::runtime_error e)
+    {
+        worked = false;
+    }
+
+    REQUIRE(worked == false);
+
+    worked = true;
+    try
+    {
+        Conversions::fromBase58Check("1LtyTmydQP28esyweRn2mJog5FkQxRh6LD", 10);
+    }
+    catch (std::runtime_error e)
+    {
+        worked = false;
+    }
+
+    REQUIRE(worked == false);
+
+}
