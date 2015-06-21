e83bd8ae580342a20dbf687ab4d4ef62350f3b1d
sgeisler <geisler.sebastian@googlemail.com>
2015-06-21 17:26:21 +0200

    added output to multisig support

--- TransactionOutput.cpp
+++ TransactionOutput.cpp
@@ -16,17 +16,38 @@ TransactionOutput::TransactionOutput(ByteArray scriptPubKey, uint64_t value)
 
 TransactionOutput::TransactionOutput(std::string addr, uint64_t value)
 {
-    ByteArray hash = Conversions::fromBase58Check(addr, 0);
-
-    if (hash.size() != 20)
-        throw std::runtime_error("This is not a valid address: " + addr);
-
-    this->scriptPubKey += OP_DUP;
-    this->scriptPubKey += OP_HASH160;
-    this->scriptPubKey += 0x14;
-    this->scriptPubKey += hash;
-    this->scriptPubKey += OP_EQUALVERIFY;
-    this->scriptPubKey += OP_CHECKSIG;
-
+    Base58CheckDecoded dec = Conversions::fromBase58Check(addr);
     this->value = value;
+
+    switch (dec.version)
+    {
+        case 0x00:
+            if (dec.data.size() != 20)
+                throw std::runtime_error("This is not a valid address: " + addr);
+
+            this->scriptPubKey += OP_DUP;
+            this->scriptPubKey += OP_HASH160;
+            this->scriptPubKey += 0x14;
+            this->scriptPubKey += dec.data;
+            this->scriptPubKey += OP_EQUALVERIFY;
+            this->scriptPubKey += OP_CHECKSIG;
+
+            break;
+
+        case 0x05:
+            if (dec.data.size() != 20)
+                throw std::runtime_error("This is not a valid address: " + addr);
+
+            this->scriptPubKey += OP_HASH160;
+            this->scriptPubKey += 0x14;
+            this->scriptPubKey += dec.data;
+            this->scriptPubKey += OP_EQUAL;
+            break;
+
+        default:
+            throw std::runtime_error("Address version not supported");
+            break;
+    }
+
+
 }
--- include/Constants.h
+++ include/Constants.h
@@ -12,6 +12,7 @@
 //OP codes
 #define OP_DUP 0x76
 #define OP_HASH160 0xa9
+#define OP_EQUAL 0x87
 #define OP_EQUALVERIFY 0x88
 #define OP_CHECKSIG 0xac
 
